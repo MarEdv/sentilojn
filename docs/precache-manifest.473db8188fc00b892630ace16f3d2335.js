34bd1b683ecb2a5fd853fac84412ef97328d5e62
Marcus Edvinsson <edvinsson@gmail.com>
2021-01-20 22:16:50 +0100

    We try it.

---
+++ docs/precache-manifest.473db8188fc00b892630ace16f3d2335.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c5f31a13572c8e29b0d586e3c2068f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e07d98f57672a38160fe",
+    "url": "/static/css/2.c595a105.chunk.css"
+  },
+  {
+    "revision": "391542f8025e5e89bd3d",
+    "url": "/static/css/main.6cf120ff.chunk.css"
+  },
+  {
+    "revision": "e07d98f57672a38160fe",
+    "url": "/static/js/2.a0196e0d.chunk.js"
+  },
+  {
+    "revision": "b49a364f830b2402690c89338ae5551d",
+    "url": "/static/js/2.a0196e0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "391542f8025e5e89bd3d",
+    "url": "/static/js/main.26ae08eb.chunk.js"
+  },
+  {
+    "revision": "cce8eb93f05396a81973",
+    "url": "/static/js/runtime-main.32fb36b3.js"
+  }
+]);
