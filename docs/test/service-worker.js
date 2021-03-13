932e4dabad7efa3655b52f9543f5a645af9c56e1
Marcus Edvinsson <edvinsson@gmail.com>
2021-03-13 22:32:12 +0100

    Value button colors and minor layout.

--- docs/test/service-worker.js
+++ docs/test/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sentilojn/test/precache-manifest.e1657576c7445d5a6a6004689dd12370.js"
+  "/sentilojn/test/precache-manifest.b09dc2463a8f5536f0cb570661d3e2d6.js"
 );
 
 self.addEventListener('message', (event) => {
